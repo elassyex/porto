114afe4523649b70a184630088c6cad8e70c5448
elassyex <103861551+elassyex@users.noreply.github.com>
2023-01-21 06:13:05 +0300

    Update settings.py

--- porto/settings.py
+++ porto/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hkm$@5x*h9z7u^l9*0-o!=q+sp*8*h^a0h-gx#vlm^4!@tl(j5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -118,10 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR , 'static'),
-)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    [BASE_DIR , 'static'],
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
