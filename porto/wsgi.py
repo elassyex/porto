ec9fe84deffff0f99109e0cb18a5fd32f1f99ed1
elassyex <103861551+elassyex@users.noreply.github.com>
2023-01-21 04:39:18 +0300

    Update wsgi.py

--- porto/wsgi.py
+++ porto/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'porto.settings')
 
-application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
