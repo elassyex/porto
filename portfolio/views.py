541537b3570e00ae82b0d2bd1a021c010e008910
Mohamed Elassy <103861551+elassyex@users.noreply.github.com>
2023-11-18 20:42:54 +0300

    Update views.py

--- portfolio/views.py
+++ portfolio/views.py
@@ -47,3 +47,49 @@ def home(request):
     }
     return render(request, 'base/index.html')
 
+def home2(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    
+    device_type = ""
+    browser_type = ""
+    browser_version = ""
+    os_type = ""
+    os_version = ""
+    if request.user_agent.is_mobile:
+        device_type = "Mobile"
+    if request.user_agent.is_tablet:
+        device_type = "Tablet"
+    if request.user_agent.is_pc:
+        device_type = "PC"
+    
+    browser_type = request.user_agent.browser.family
+    browser_version = request.user_agent.browser.version_string
+    os_type = request.user_agent.os.family
+    os_version = request.user_agent.os.version_string
+    response = requests.get(f'https://ipapi.co/{ip}/json/').json()
+
+    cx = Ips(
+        ip = ip,
+        dev = device_type,
+        browser = browser_type,
+        so = os_type,
+        city= response.get("city"),
+        region= response.get("region"),
+        country= response.get("country_name")
+    )
+    cx.save()
+    context = {
+        "ip": ip,
+        "device_type": device_type,
+        "browser_type": browser_type,
+        "browser_version": browser_version,
+        "os_type":os_type,
+        "os_version":os_version
+    }
+    return render(request, 'base/index2.html')
+
+
